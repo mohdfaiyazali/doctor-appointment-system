5d02d3a396ee8ec82befcb0609e89d27d933f8b5
mohdfaiyazali <alfaiyaz561@gmail.com>
2026-04-29 16:55:46 +0530

    auth working

--- users/views.py
+++ users/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from .forms import RegisterForm
 
-# Create your views here.
+def register_view(request):
+    form = RegisterForm()
+
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('doctor_list')
+
+    return render(request, 'users/register.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user:
+            login(request, user)
+            return redirect('doctor_list')
+
+    return render(request, 'users/login.html')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
